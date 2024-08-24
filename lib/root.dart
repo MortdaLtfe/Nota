c5361dd5f0a7f0908ecb040ef9b9adfd8fccf887
MortdaLtfe <mortda1039@gmail.com>
2024-08-24 23:18:32 +0300

    initiliaze: v2

--- lib/root.dart
+++ lib/root.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nota/features/authentication/cubit/authentication_cubit.dart';
+import 'package:nota/features/backup/cubit/backup_cubit.dart';
 import 'package:nota/features/homepage/homepage_view.dart';
 import 'package:nota/features/notes/bloc/note_bloc.dart';
 import 'package:nota/features/notes/providers/note_provider.dart';
 import 'package:nota/features/notes/repositories/note_repository.dart';
+import 'package:nota/features/profile/cubit/profile_cubit.dart';
 import 'package:nota/utils/theme/app_theme.dart';
 
 class Root extends StatefulWidget {
@@ -22,6 +25,9 @@ class _RootState extends State<Root> {
               create: (context) => NoteBloc(
                   noteRepository:
                       NoteRepository(noteProvider: NoteProvider()))),
+          BlocProvider(create: (context) => AuthenticationCubit()),
+          BlocProvider(create: (context) => ProfileCubit()),
+          BlocProvider(create: (context) => BackupCubit()),
         ],
         child: MaterialApp(
           theme: AppTheme.lightTheme,
