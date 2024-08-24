31dd998bd368f24ec93540af81655558a2c43bcb
MortdaLtfe <mortda1039@gmail.com>
2024-08-24 23:27:53 +0300

    Refactor: Delete some lines

--- lib/features/notes/views/add_note.dart
+++ lib/features/notes/views/add_note.dart
@@ -37,15 +37,20 @@ class _AddNoteState extends State<AddNote> {
               const IconThemeData(color: Colors.white54, size: 28),
           iconTheme: const IconThemeData(color: Colors.white54),
           backgroundColor: const Color(0xff1f1f1f),
-          leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back_ios_new_outlined,
+          leading: Row(children: [
+            IconButton(
+              icon: const Icon(
+                Icons.arrow_back_ios_new_outlined,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+          ]),
+          title: Text(
+            "Add Note",
+            style: Theme.of(context).textTheme.headlineSmall,
           ),
-          title: const Text("Edit Note"),
           actions: [
             IconButton(
               icon: const Icon(Icons.save),
